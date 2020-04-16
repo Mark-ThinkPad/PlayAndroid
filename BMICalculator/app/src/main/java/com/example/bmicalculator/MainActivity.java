44e0f5c79ed24d52f8997b4be907e70731ccda76
Mark-ThinkPad <1372469698@qq.com>
2020-04-17 01:47:57 +0800

    play and learn

--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.bmicalculator;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialToolbar mt = findViewById(R.id.topAppBar);
+        mt.setNavigationOnClickListener(new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                TextView tv = findViewById(R.id.textView);
+                tv.setText(R.string.clicked);
+            }
+        });
     }
 }
