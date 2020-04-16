d5a5b81fbe154f6ca8caf02c573189a8c1ad7c75
mark-thinkpad <1372469698@qq.com>
2020-04-16 11:30:55 +0800

    play and learn

--- Layout/app/src/main/java/com/example/layout/MainActivity.java
+++ Layout/app/src/main/java/com/example/layout/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.layout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -8,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // button1 click
+    public void button1_click(View view) {
+
+    }
+
+    // button2 click
+    public void button2_click(View view) {
+
+    }
+
+    // button3 click
+    public void button3_click(View view) {
+
     }
 }
