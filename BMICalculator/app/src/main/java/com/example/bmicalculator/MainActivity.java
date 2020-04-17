2d655d7737f5d6369a045786a7c0da69fdf43134
Mark-ThinkPad <1372469698@qq.com>
2020-04-17 10:08:54 +0800

    play and learn

--- BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.bmicalculator;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,12 +17,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MaterialToolbar mt = findViewById(R.id.topAppBar);
+        final DrawerLayout dl = findViewById(R.id.dl);
+        final NavigationView nv = findViewById(R.id.nav_view);
+        final MaterialToolbar mt = findViewById(R.id.topAppBar);
+        final TextView tv = findViewById(R.id.textView);
+
         mt.setNavigationOnClickListener(new View.OnClickListener () {
             @Override
             public void onClick(View v) {
-                TextView tv = findViewById(R.id.textView);
-                tv.setText(R.string.clicked);
+                dl.openDrawer(nv);
+            }
+        });
+
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        tv.setText(R.string.clicked);
+                    default:
+                        dl.closeDrawers();
+                }
+
+                return false;
             }
         });
     }
