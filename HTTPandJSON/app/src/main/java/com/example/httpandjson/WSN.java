fe6bb327258f9e286b5636ab0a9b85af6493c288
Mark-ThinkPad <1372469698@qq.com>
2020-05-04 17:10:49 +0800

    play and learn

--- HTTPandJSON/app/src/main/java/com/example/httpandjson/WSN.java
+++ HTTPandJSON/app/src/main/java/com/example/httpandjson/WSN.java
@@ -1,4 +1,48 @@
 package com.example.httpandjson;
 
 public class WSN {
+
+    private String GroupID;
+    private String XH;
+    private String Value;
+    private String Date;
+
+    public WSN(String groupID, String xh, String value, String date) {
+        this.GroupID = groupID;
+        this.XH = xh;
+        this.Value = value;
+        this.Date = date;
+    }
+
+    public String getGroupID() {
+        return GroupID;
+    }
+
+    public String getXH() {
+        return XH;
+    }
+
+    public String getValue() {
+        return Value;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setGroupID(String groupID) {
+        GroupID = groupID;
+    }
+
+    public void setXH(String xh) {
+        XH = xh;
+    }
+
+    public void setValue(String value) {
+        Value = value;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
 }
--- HTTPandJSON/app/src/main/java/com/example/httpandjson/WSNAdapter.java
+++ HTTPandJSON/app/src/main/java/com/example/httpandjson/WSNAdapter.java
@@ -1,12 +1,41 @@
 package com.example.httpandjson;
 
 import android.content.Context;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 import androidx.annotation.NonNull;
 
-public class WSNAdapter extends ArrayAdapter {
-    public WSNAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+import java.util.LinkedList;
+
+public class WSNAdapter extends BaseAdapter {
+
+    private LinkedList<WSN> data;
+    private Context context;
+
+    public WSNAdapter(LinkedList<WSN> data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
     }
 }
